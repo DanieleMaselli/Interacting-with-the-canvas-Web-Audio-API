3ffbf04d3295ee61cd798991089b9ecc3503e1c9
Daniele Maselli <danymase@gmail.com>
2018-04-21 11:33:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,78 @@
+//select the first match
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; 
+//2 dimensional context we are gonna to draw
+var context = canvas.getContext('2d');
+var analyser;
+
+
+//////////Audio Part ////////////////
+var audio = new Audio();
+audio.src = 'audio.mp3';
+//create an analyser node 
+var audioContext = new AudioContext();
+analyser = audioContext.createAnalyser();
+//source 
+var source = audioContext.createMediaElementSource(audio);
+source.connect(analyser);
+analyser.connect(audioContext.destination);
+analyser.fftSize = 1024;
+
+var buffer = analyser.frequencyBinCount / 2;
+var data   = new Uint8Array(buffer);
+audio.play();
+//audio.play();
+/////////////////////////////////////
+
+var mouse = {
+	x: undefined,
+	y: undefined
+}
+
+function draw() {
+	var x;
+    var y;
+    var delta = canvas.width / buffer; 
+    var fx = 0;
+    var radius = 200;
+    x = mouse.x;
+    y = mouse.y;
+
+    for(var i = 0; i < buffer; i++) {
+    	var v = data[i] / 2;
+    	if(v === 0) continue;
+        context.beginPath();
+        
+		context.arc(x, y, v, fx, v);
+		//context.lineTo(fx, v);
+		context.strokeStyle = "white";
+		context.stroke();
+		fx += delta;
+		console.log(v);
+	
+	}
+	
+}
+
+function clear() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function animate(){
+	requestAnimationFrame(animate);
+	analyser.getByteTimeDomainData(data);
+clear();	
+draw();
+
+}
+
+window.addEventListener('mousemove', 
+function(event) {
+	
+	mouse.x = event.x;
+	mouse.y = event.y;
+});
+
+animate();
 
